85d8c409c148c2c4e730465071b698d87c359f8c
Rocio Liliana Estevez Fuentes <ilianaestevez@MacBook-Air-de-Rocio.local>
2026-06-18 18:17:37 -0600

    Prepare backend for production

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,14 @@ import { createGame, joinGame, getGame, startGame, getCurrentQuestion,   submitA
 
 
 const app = express();
-const PORT = 3001;
+const PORT =
+  process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on ${PORT}`
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
