b7f2e1939a7c205fb07faf35de9c4e24e42203c6
Rocio Liliana Estevez Fuentes <ilianaestevez@MacBook-Air-de-Rocio.local>
2026-06-24 19:44:00 -0600

    MVP 2: environment configuration

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,4 +2,8 @@ export const API =
   import.meta.env.VITE_API_URL ??
   "http://localhost:3001";
 
-console.log("🌎 API:", API);
+console.log("MODE:", import.meta.env.MODE);
+console.log("PROD:", import.meta.env.PROD);
+console.log("DEV:", import.meta.env.DEV);
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+console.log("API:", API);
